b85f49a68aa536cbcff30085e8fdaa111991fe5f
gw1106 <gw1106@naver.com>
2023-03-13 10:37:53 +0900

    localCommit1 연습문제1, 자가진단1 실습완료

---
+++ ch13.py
@@ -0,0 +1,28 @@
+### 리스트3
+
+
+
+
+a = range(1, 11)
+print(type(a))
+print(list(a))
+
+###연습문제1
+##print("원소가 모두 0:", [0 for _ in range(10)])
+##print("또 다른 방법:", [0]*10)
+##print("원소가 모두 'Hi':", ['Hi' for _ in range(10)])
+##print("1부터 10까지:", [i for i in range(1, 11)])
+##print("또 다른 방법:", list(range(1, 11)))
+##print("5부터 15까지의 홀수:", [i for i in range(5, 16, 2)])
+##print("60부터 40까지의 3의 배수:", [i for i in range(60, 39, -3)])
+##print("A부터 F까지의 알파벳:", [chr(i) for i in range(ord('A'), ord('F')+1)])
+##
+                        
+### 자가진단1
+##n = int(input())
+##lis = []
+##
+##for i in range(1, n + 1):
+##    lis.append(i * i)
+##
+##print(lis)
